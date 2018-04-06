05eae25f68e5f427deb72404bb96e0002d89457c
highsare <highsare@naver.com>
2018-04-06 21:24:51 +0900

    04-06 문희재 - 1차 모션입력 이벤트 연동

--- RhythmiCal/src/main/java/com/beatoven/rhythmical/controller/StageController.java
+++ RhythmiCal/src/main/java/com/beatoven/rhythmical/controller/StageController.java
@@ -1,10 +1,45 @@
 package com.beatoven.rhythmical.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /*스테이지에서 호출되는 모든 서버로의 요청은 이곳에서 진행됩니다.*/
 
 @Controller
 public class StageController {
+	
+	private String motionBox;
+	private boolean isUsed = false;
 
+	//모션 값 받아오기
+	@ResponseBody
+	@RequestMapping(value="sendMotion", method = RequestMethod.POST)
+	public String receiveMotion(String request,String motion) {
+		motionBox = motion;
+		System.out.println(motionBox);
+		return "received";
+	}
+	
+	//모션 값 페이저로 전달
+	@ResponseBody
+	@RequestMapping(value="requestMotion", method = RequestMethod.POST)
+	public String requestMotion() {
+		if(motionBox != null) {
+			if (isUsed) {
+				System.out.println("[Motion empty]");
+				motionBox = null;
+				isUsed = false;
+			}else {
+				System.out.println("Motion requested"+motionBox);
+				isUsed = true;
+				return motionBox;	
+			}
+		}
+		return "NOTHING";
+	}
 }
