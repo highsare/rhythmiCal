1f05018891c59cc0b0847189c83b5a7e06bc7e56
Daniel-kr-mx-jp <36183007+Daniel-kr-mx-jp@users.noreply.github.com>
2018-04-26 21:43:17 +0900

    bossbuff 업데이트중

--- RhythmiCal/src/main/webapp/resources/Phaser/stage/bosses.js
+++ RhythmiCal/src/main/webapp/resources/Phaser/stage/bosses.js
@@ -9,6 +9,9 @@ function bossesJump(bossEntity) {
     //1초마다 스프라이트 점프 이미지
     animBeatoven = beatoven.animations.add('jump',[0,1],2,false);
     animBeatoven.play('jump');
+    
+    //비트마다 스킬을 사용한다.
+    //bossUseBuffSkill(bossEntity);
 }
 
 //노비토 객체
@@ -18,6 +21,7 @@ function Nobeato(game){
 	this.health = 50;
 	this.maxHealth = 50;
 	this.status = "STAY"; //STAY , MOVE , STUN, DIE, IMUNE
+	this.skillPercentage = 0;
 	
 	this.nobeatoSprite = game.add.sprite(1420, game.world.centerY, 'nobeato', 5);
 	this.nobeatoSprite.scale.set(4);
@@ -96,7 +100,42 @@ function attackedMotionBosses(bossSprite){
 	
 }
 
-//몬스터 객체 유닛 데미지
+//보스몹 객체 유닛 데미지
 function hitBoss(bossEntity, damage){
 	bossEntity.damage(damage);
+}
+
+//보스몹이 랜덤으로 스킬을 사용하고 랜덤으로 라인을 지정해 버프를 건다.
+function bossUseBuffSkill(bossEntity){
+	var skillPercentage = bossEntity.skillPercentage;
+	//1~10번중 랜덤으로 번호 생성해서 random에 할당
+	var random = game.rnd.integerInRange(1, 10);
+	if (random <= skillPercentage) {
+		var randomLineBuff = game.rnd.integerInRange(0, 2);
+		switch (randomLineBuff) {
+		case 0:
+			for (var i = 0; i < monstersA.length; i++) {
+				if (monstersA[i].status != "STAY") {
+					monstersA[i].speed++;
+				}
+			}
+			break;
+		case 1:
+			for (var i = 0; i < monstersB.length; i++) {
+				if (monstersB[i].status != "STAY") {
+					monstersB[i].speed++;
+				}
+			}
+			break;
+		case 2:
+			for (var i = 0; i < monstersC.length; i++) {
+				if (monstersC[i].status != "STAY") {
+					monstersC[i].speed++;
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
--- RhythmiCal/src/main/webapp/resources/Phaser/stage/monsters.js
+++ RhythmiCal/src/main/webapp/resources/Phaser/stage/monsters.js
@@ -204,8 +204,6 @@ function singleJump (unit, maximumHeightOnAttackLine, destination) {
 	//이동 거리
 	game.add.tween(unit.monsterSprite).to({ x: destination }, 600, 'Linear', true, 0);
 	game.add.tween(unit.monsterHealthbar).to({ x: destination }, 600, 'Linear', true, 0);
-
-	hitMonster(unit, 1); //테스트를 위해서 임으로 주는 데미지
 	//다음 목적지를 설정한다.
 	unit.lineX = destination;
 }
@@ -228,7 +226,8 @@ function attackLine(unitArray, damage){
 	for(var i = 0; i < unitArray.length; i++){
 		var unit = unitArray[i];
 		if(unit.lineXIndex != 0){
-			if (unit.monsterNum == 3) {//몬스터 번호가 3인 방패몬스터를 만나면 뒤의 몬스터에게 데미지를 먹이지 않는다.
+			if (unit.monsterNum == 3 && unit.status != "DIE") {//몬스터 번호가 3인 방패몬스터를 만나면 뒤의 몬스터에게 데미지를 먹이지 않는다.
+				hitMonster(unit, damage);
 				break;
 			}
 			hitMonster(unit,damage);
