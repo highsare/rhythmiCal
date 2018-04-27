6ebe49d62cff36572a1a4e04f2543753e51d6226
RHOJUNGHUN <noelfania@naver.com>
2018-04-27 21:28:41 +0900

    DB연결된 스토리틀

--- RhythmiCal/src/main/java/com/beatoven/rhythmical/interfaces/StoryInterface.java
+++ RhythmiCal/src/main/java/com/beatoven/rhythmical/interfaces/StoryInterface.java
@@ -1,10 +1,16 @@
 package com.beatoven.rhythmical.interfaces;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.beatoven.rhythmical.vo.Story;
 
 /*스토리 관련 인터페이스*/
 
 @Mapper
 public interface StoryInterface {
 
+	public ArrayList<Story> selectStory(int storynum);
+	
 }
