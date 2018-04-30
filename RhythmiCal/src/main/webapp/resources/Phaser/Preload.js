40e5b40f7bce765a16c8945501ce5bd0b12c4467
highsare <highsare@naver.com>
2018-05-01 01:15:14 +0900

    스토리 DB 연결작업 (아직 테스트안함)

--- RhythmiCal/src/main/webapp/resources/Phaser/Preload.js
+++ RhythmiCal/src/main/webapp/resources/Phaser/Preload.js
@@ -451,6 +451,7 @@ function setResources (state){
 	}else if (state == "Story") {
 		//Story assets , contentNum required
 	    //스토리 실행
+		storyNum = contentNum;
 		game.state.start("Story");
 		
 		
